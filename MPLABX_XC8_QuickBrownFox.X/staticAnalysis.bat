2f8cea660b822bbd98f1adcdfe22545664722231
Richard.Killeen <Richard.Killeen@eielectronics.ie>
2021-03-31 14:20:56 +0100

    static analysis batch file

---
+++ MPLABX_XC8_QuickBrownFox.X/staticAnalysis.bat
@@ -0,0 +1,13 @@
+set ldra=C:\LDRA_toolsuite\
+set proj=%cd%
+
+rem %ldra%contbbuildimport.exe -build_cmd=Build.bat -startin_dir="%proj%" -settings="C:\LDRA_Toolsuite\Compiler_spec\Microchip\xc8\xc8_tbmakelogparser.dat" -build -quit
+
+start /wait %ldra%contestbed testProject -create_set=system -1q
+
+forfiles /s /m *.c /c "cmd /c start /wait %ldra%contestbed testProject -add_set_file=@path -1q"
+forfiles /s /m *.h /c "cmd /c start /wait %ldra%contestbed testProject -add_set_file=@path -1q"
+
+start /wait %ldra%contestbed testProject -112a34567q -generate_code_review=HTML -publish_to_dir=%proj%
+
+if errorlevel = 1 EXIT /B %ERRORLEVEL%
